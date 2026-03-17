a34cab432d2fb21d6348ff66aaa67adbc52143b4
Cjjamieson <232105809+cjjamieson1983@users.noreply.github.com>
2026-03-17 15:44:29 -0400

    Add Lab1a, Lab1b, Lab1c, Lab1c.A, Lab1c.B, Lab1c.C, Lab1c.D, Lab1c.e, Lab1c.F, and Lab2a, project files

---
+++ lab1c.C/outputs.tf
@@ -0,0 +1,33 @@
+# Explanation: Outputs are your mission report—what got built and where to find it.
+output "lab1c_vpc_id" {
+  value = aws_vpc.lab1c_vpc01.id
+}
+
+output "lab1c_public_subnet_ids" {
+  value = aws_subnet.lab1c_public_subnets[*].id
+}
+
+output "lab1c_private_subnet_ids" {
+  value = aws_subnet.lab1c_private_subnets[*].id
+}
+
+output "lab1c_ec2_instance_id" {
+  value = aws_instance.lab1c_ec2.id
+}
+
+output "lab1c_rds_endpoint" {
+  value = aws_db_instance.lab1c_rds01.address
+}
+
+output "lab1c_sns_topic_arn" {
+  value = aws_sns_topic.lab1c_sns_topic01.arn
+}
+
+output "lab1c_log_group_name" {
+  value = aws_cloudwatch_log_group.lab1c_log_group01.name
+}
+
+output "lab1c_apex_url_https" { 
+  value = "https://${var.domain_name}" 
+  }
+
