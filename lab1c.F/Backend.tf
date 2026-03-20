2deef24ce99b8d0cccb7cf67547388a31e1e82d6
jastek69 <89563369+jastek69@users.noreply.github.com>
2026-03-20 00:55:29 -0700

    README cleanup

--- lab1c.F/Backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "s3states2026"
-    key    = "State/30226/terraform.tfstate"
-    region = "us-east-2"
-  }
-}
--- lab2a/providers.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region = var.aws_region
-}
-
-
-provider "aws" {
-  alias  = "use1"
-  region = "us-east-1"
-}
